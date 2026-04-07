82d372036186d1e9c14f5bd7288bbeec57cfff49
Ernesto Cabral <ernestocabralm@gmail.com>
2026-04-07 16:07:59 -0500

    Update data.js

--- data.js
+++ data.js
@@ -3,6 +3,7 @@ const posts = [
     id: "P00002",
     fecha: "2026-04-04T16:23:00",
     views: 33500,
+    impacto: 0.92,
     plataforma: "X",
     url: "https://x.com/gianca2709/status/2040540735687016624"
   },
@@ -10,98 +11,8 @@ const posts = [
     id: "P00013",
     fecha: "2026-04-04T16:32:00",
     views: 8,
+    impacto: 0.92,
     plataforma: "X",
     url: "https://x.com/KonstantAdriana/status/2040543027584761946"
-  },
-  {
-    id: "P00006",
-    fecha: "2026-04-04T16:40:00",
-    views: 340,
-    plataforma: "X",
-    url: "https://x.com/FernandoGuineaN/status/2040545141182882121"
-  },
-  {
-    id: "P00007",
-    fecha: "2026-04-04T16:41:00",
-    views: 184,
-    plataforma: "X",
-    url: "https://x.com/FernandoGuineaN/status/2040545216428433839"
-  },
-  {
-    id: "P00011",
-    fecha: "2026-04-04T16:56:00",
-    views: 1969,
-    plataforma: "X",
-    url: "https://x.com/PianoNocturno/status/2040549049632264425"
-  },
-  {
-    id: "P00010",
-    fecha: "2026-04-04T17:43:00",
-    views: 4949,
-    plataforma: "X",
-    url: "https://x.com/PulmonDeSusan7/status/2040561025565540801"
-  },
-  {
-    id: "P00001",
-    fecha: "2026-04-04T18:36:00",
-    views: 53100,
-    plataforma: "X",
-    url: "https://x.com/AlcortaLourdes/status/2040574376777572582"
-  },
-  {
-    id: "P00003",
-    fecha: "2026-04-04T18:53:00",
-    views: 94,
-    plataforma: "X",
-    url: "https://x.com/jlcontrerasr76/status/2040578528102482255"
-  },
-  {
-    id: "P00008",
-    fecha: "2026-04-04T19:12:00",
-    views: 40500,
-    plataforma: "X",
-    url: "https://x.com/PianoNocturno/status/2040583234577396187"
-  },
-  {
-    id: "P00005",
-    fecha: "2026-04-04T19:24:00",
-    views: 166,
-    plataforma: "X",
-    url: "https://x.com/Urano55/status/2040586343885988150"
-  },
-  {
-    id: "P00015",
-    fecha: "2026-04-04T21:34:00",
-    views: 472,
-    plataforma: "X",
-    url: "https://x.com/AlePeru2021/status/2040618979958399271"
-  },
-  {
-    id: "P00004",
-    fecha: "2026-04-04T23:19:00",
-    views: 111,
-    plataforma: "X",
-    url: "https://x.com/josferperu/status/2040645523124756921"
-  },
-  {
-    id: "P00014",
-    fecha: "2026-04-05T05:12:00",
-    views: 83,
-    plataforma: "X",
-    url: "https://x.com/FelicianoGalvan/status/2040734359955669043"
-  },
-  {
-    id: "P00012",
-    fecha: "2026-04-05T08:43:00",
-    views: 58,
-    plataforma: "X",
-    url: "https://x.com/charlieolle/status/2040787537103352097"
-  },
-  {
-    id: "P00009",
-    fecha: "2026-04-05T14:23:00",
-    views: 16400,
-    plataforma: "X",
-    url: "https://x.com/wanupa/status/2040691873124139183"
   }
 ];
